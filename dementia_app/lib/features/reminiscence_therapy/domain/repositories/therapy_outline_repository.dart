10d13a0cb6dc0effe91dce64c7de479c06d59bc7
Sasindu Gunawardana <sasindudananjaya2000@gmail.com>
2025-01-03 18:47:45 +0530

    initial commit

--- dementia_app/lib/features/reminiscence_therapy/domain/repositories/therapy_outline_repository.dart
+++
@@ -1,7 +0,0 @@
-
-import '../entities/therapy_outline.dart';
-
-abstract class TherapyOutlineRepository {
-  Future<List<TherapyOutline>> getCompletedTherapyOutlines();
-  Future<List<TherapyOutline>> getAllTherapyOutlines();
-}
